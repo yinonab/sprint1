e137395fad1974176353da9d5acd2b9f186f1698
yinonab <88625573+yinonab@users.noreply.github.com>
2023-08-03 19:20:44 +0300

    adding timer

--- js/main.js
+++ js/main.js
@@ -18,7 +18,7 @@ var gGame = {
 }
 // var gCell = null
 var gMinesNumbers = gLevel.MINES
-var gTimerInterval
+// var gTimerInterval
 var gMarkedCount = 0
 var gShownCount = 0
 var gBoardCells = gLevel.SIZE ** 2
@@ -26,12 +26,12 @@ var gBoardCells = gLevel.SIZE ** 2
 
 
 function onInit() {
-    clearTimer ()
-    gGame.isOn=true
+    stopTimer()
+    gGame.isOn = true
     gShownCount = 0
     gBoard = createBoard()
     renderBoard()
-     updateScore(gMinesNumbers)
+    updateScore(gMinesNumbers)
     // updateMines(gMinesNumbers)
 
     // disableRightClick()
@@ -77,7 +77,8 @@ function renderBoard() {
     elCells.innerHTML = strHTML
 }
 function increaseLevel() {
-    clearTimer ()
+    stopTimer()
+    resetTimer()
     gGame.markedCount = 0
     gGame.shownCount = 0
     gGame.secsPassed = 0
@@ -85,13 +86,14 @@ function increaseLevel() {
     gLevel.MINES += 1
     updateRestButton('😀')
     // gShownCount = 0
-     gMinesNumbers = gLevel.MINES
-     updateScore(gMinesNumbers)
+    gMinesNumbers = gLevel.MINES
+    updateScore(gMinesNumbers)
     // updateMines(gMinesNumbers)
     onInit()
 }
 function decreaseLevel() {
-    clearTimer ()
+    stopTimer()
+    resetTimer()
     gGame.markedCount = 0
     gGame.shownCount = 0
     gGame.secsPassed = 0
@@ -105,7 +107,8 @@ function decreaseLevel() {
     onInit()
 }
 function RestartGame() {
-    clearTimer ()
+    stopTimer()
+    resetTimer()
     gGame.markedCount = 0
     gGame.shownCount = 0
     gGame.secsPassed = 0
@@ -118,18 +121,20 @@ function RestartGame() {
 }
 function checkGameOver() {
     // console.log('gGame:', gGame)
-     if (gMinesNumbers === 0) {
-         updateRestButton('You Lose !! 😡')
-         gGame.isOn=false
-         clearTimer ()
-         
-         
+    if (gMinesNumbers === 0) {
+        updateRestButton('You Lose !! 😡')
+        gGame.isOn = false
+        stopTimer()
+        resetTimer()
+
+
     }
-     else if (gGame.shownCount-gGame.markedCount=== gLevel.SIZE ** 2 - gLevel.MINES) {
-         updateRestButton('You Win !! 👑')
-         gGame.isOn=false
-         clearTimer ()
-         
+    else if (gGame.shownCount - gGame.markedCount === gLevel.SIZE ** 2 - gLevel.MINES) {
+        updateRestButton('You Win !! 👑')
+        gGame.isOn = false
+        stopTimerAndSave()
+        resetTimer()
+
     }
 }
 function CellMarked(event, elcell, i, j) {
@@ -147,8 +152,8 @@ function onCellClicked(elCell, i, j) {
     if (!gGame.isOn) return
     var currCell = gBoard[elCell.dataset.i][elCell.dataset.j]
     elCell.style.backgroundColor = "white"
-    if ( gGame.shownCount == 0 && gGame.secsPassed === 0) {
-        gTimerInterval = setInterval(updateTimer, 1000);
+    if (gGame.shownCount == 0 && gGame.secsPassed === 0) {
+        startTimer()
         gGame.shownCount += 1
         // debugger
         elCell.innerText = '❤'
@@ -164,7 +169,7 @@ function onCellClicked(elCell, i, j) {
                 j = +elInCell.dataset.j
                 var mine = gBoard[i][j]
                 var nigNum = setMinesNegsCount(gBoard, i, j)
-                if (nigNum > 0 && mine.isMine === false ) {
+                if (nigNum > 0 && mine.isMine === false) {
                     //elInCell.classList.add('cellRevile')
                     elInCell.minesAroundCount = nigNum
                     elInCell.innerText = nigNum
@@ -191,8 +196,8 @@ function onCellClicked(elCell, i, j) {
     else if (currCell.isMine) {
         gGame.shownCount += 1
         // debugger
-         gMinesNumbers--
-         updateScore(gMinesNumbers)
+        gMinesNumbers--
+        updateScore(gMinesNumbers)
         // console.log('gMinesCount:', gMinesCount)
         // updateShownCount(1)
         currCell.isShown = true
@@ -236,14 +241,14 @@ function colorMinesNegsAround(board, rowIdx, colIdx) {
             if (i === rowIdx && j === colIdx) continue
             if (j < 0 || j >= board[0].length) continue
             var currCell = board[i][j]
-            if (!currCell.isMine && !currCell.isMarked&&!currCell.isShown) {
+            if (!currCell.isMine && !currCell.isMarked && !currCell.isShown) {
                 var elInCell = document.querySelector(`[data-i="${i}"][data-j="${j}"]`)
                 elInCell.style.backgroundColor = "white"
-                currCell.isShown=true
+                currCell.isShown = true
                 gGame.shownCount += 1
                 gGame.secsPassed += 1
             }
-            
+
         }
         // gGame.shownCount += 1
     }
@@ -305,31 +310,76 @@ function updateMines(gMinesCount) {
     var elLive = document.querySelector('.mines')
     elLive.innerText = 'Mines On The Board  : ' + gMinesCount
 }
-function clearTimer (){
-    clearInterval(gTimerInterval)
-    const timerElement = document.querySelector('.timer');
-    timerElement.innerText='00:00'
-}
+
 // Set the initial time in seconds
-let currentTime = 0;
+var currentTime = 0;
+var timerInterval;
+var timerRunning = false;
+var savedTime = localStorage.getItem("savedTime"); 
 
-// Get the timer element
 const timerElement = document.querySelector('.timer');
-
-// Start the timer
-
+const startButton = document.querySelector('.startButton');
+const stopButton = document.querySelector('.stopButton');
+const resetButton = document.querySelector('.resetButton');
+const clearStorageButton = document.querySelector('.clearStorageButton');
+const savedTimeElement = document.getElementById("savedTimeValue"); // Update the saved time value
 
 function updateTimer() {
-    const minutes = Math.floor(currentTime / 60);
-    const seconds = currentTime % 60;
-  
-    // Display the current time in "MM:SS" format
-    timerElement.textContent = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
-  
-    // Increment the time
-    currentTime++;
+  const minutes = Math.floor(currentTime / 60);
+  const seconds = currentTime % 60;
+  timerElement.textContent = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+  currentTime++;
+}
+function resetTimer() {
+    clearInterval(timerInterval);
+    timerRunning = false;
+    currentTime = 0;
+    timerElement.textContent = "00:00"; // Reset the timer display
+  }
+  function clearLocalStorage() {
+    localStorage.clear();
+    savedTime = null;
+    savedTimeElement.textContent = "Not saved"; // Reset saved time display
   }
   
 
+function startTimer() {
+  if (!timerRunning) {
+    clearInterval(timerInterval);
+    timerInterval = setInterval(updateTimer, 1000);
+    timerRunning = true;
+  }
+}
+
+function stopTimerAndSave() {
+  clearInterval(timerInterval);
+  timerRunning = false;
+  saveTime();
+}
+function stopTimer() {
+    clearInterval(timerInterval);
+    timerRunning = false;
+  }
+function saveTime() {
+    // If there's no saved time or the new time is shorter, update the saved time
+    if (savedTime === null || currentTime < savedTime) {
+      savedTime = currentTime;
+      localStorage.setItem("savedTime", savedTime);
+    }
+    savedTimeElement.textContent = `${savedTime} seconds`; // Update saved time display
+  }
+  
+  startButton.addEventListener("click", startTimer);
+  stopButton.addEventListener("click", stopTimerAndSave);
+  resetButton.addEventListener("click", resetTimer);
+  clearStorageButton.addEventListener("click", clearLocalStorage);
 
 
+  
+  // Automatically start the timer when the page loads
+  
+  // Load saved time and update display
+  if (savedTime !== null) {
+    savedTimeElement.textContent = `${savedTime} seconds`;
+  }
+  
